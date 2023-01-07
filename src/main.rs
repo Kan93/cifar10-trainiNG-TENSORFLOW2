828ce8039f527438bbe61cb105142c2d1bb19d07
Kan93 <92035832+Kan93@users.noreply.github.com>
2023-01-07 18:27:32 +0000

    [maven-release-plugin] prepare release 2.0

--- src/main.rs
+++ src/main.rs
@@ -6,3 +6,13 @@ use std::io::BufRead;
 use std::sync::atomic::{AtomicUsize, Ordering};
 use std::sync::{Arc, Mutex};
 use std::thread;
+use std::time::{Duration, Instant};
+
+use cn_stratum::client::{
+    ErrorReply, Job, JobAssignment, MessageHandler, PoolClient, PoolClientWriter, RequestId,
+};
+use yellowsun::{Algo, AllocPolicy, Hasher};
+
+use byteorder::{ByteOrder, LE};
+use core_affinity::CoreId;
+use log::*;
